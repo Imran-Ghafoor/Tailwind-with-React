843eb6110fc216c4dff6a7a31f0fdd160d191bd4
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-12-12 23:34:10 +0500

    tailwind css

---
+++ tailwindcss/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-600 text-white text-4xl h-screen'>Hello Tailwind</h1>
+    </>
+  )
+}
+
+export default App
