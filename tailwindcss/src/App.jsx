4ed20b24ef9c5f0c0fa63aa5e51c3afe6be10668
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-12-16 23:16:17 +0500

    tailwind css

--- tailwindcss/src/App.jsx
+++ tailwindcss/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
+      <h1> Tailwind css</h1>
       {/* <h1 className="text-white text-1xl mb-2">Hello Tailwind</h1> */}
       <div className="">
         <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
